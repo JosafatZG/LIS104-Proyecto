9ffa847f5e2fee51020a085ba97b9a2a060787a0
Melvin Robles <56745314+Melvin-Robles@users.noreply.github.com>
2022-02-28 01:07:39 -0600

    Categorias

---
+++ mvc/views/login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../assets/css/estilos.css">
+    <title>Inicio/Resgistro</title>
+	<meta name="robots" content="noindex">
+	<link  rel="icon"   href="https://imgur.com/Zdbcigj.png" type="image/png" />
+	<link rel="canonical" href="https://codepen.io/ig_design/pen/KKVQpVP?editors=1111">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+	<script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeConsoleRunner-d0f3648046d2aaca07bd0037b9e061a26c74a8a999b75672ad6a638cca641472.js"></script>
+	<script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRefreshCSS-4793b73c6332f7f14a9b6bba5d5e62748e9d1bd0b5c52d7af6376f3d1c625d7e.js"></script>
+	<script src="https://cpwebassets.codepen.io/assets/editor/iframe/iframeRuntimeErrors-4f205f2c14e769b448bcf477de2938c681660d5038bc464e3700256713ebe261.js"></script>
+
+</head>
+<body class="Back" >
+   
+	<a href="#" class="Back">⇠ Regresar</a>
+
+	<div class="section">
+
+		<div class="container">
+
+			<div class="row full-height justify-content-center">
+
+				<div class="col-12 text-center align-self-center py-5">
+					<div class="section pb-5 pt-5 pt-sm-2 text-center">
+						<h6 class="mb-0 pb-3"><span>Iniciar Sesión</span><span>Registrarse</span></h6>
+			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+			          	<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="colortitle">Iniciar Sesión</h4>
+											<div class="form-group">
+												<input type="email" name="logemail" class="form-style" placeholder="Usuario" id="logusuario" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Contraseña" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="boton">Enviar</a>   		
+				      					</div>
+			      					</div>
+			      				</div>
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="colortitle">Registrarse</h4>
+												
+											<div class="form-group mt-2">
+												<input type="email" name="logemail" class="form-style" placeholder="Usuario" id="logusuario" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Contraseña" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="boton">Enviar</a>
+											
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+
+
+
+</body>
+</html>
