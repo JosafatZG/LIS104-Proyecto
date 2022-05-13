557173a82f83bfa3b5453459460772bd79b16e5c
Melvin Robles <56745314+Melvin-Robles@users.noreply.github.com>
2022-05-13 10:21:44 -0600

    cambios finales

--- mvc/views/pedidos/ordencombo.php
+++ mvc/views/pedidos/ordencombo.php
@@ -11,6 +11,29 @@
 	</head>
 	
 	<body>
+
+	<header>
+   
+
+        <img src="assets/images/logo.png" class="logo">
+        <form class="menu">
+            <input type="button" id="inicio" name="inicio" value="Inicio" class="submit" onclick="location.href='?c=pedidos';" />
+            <input type="button" id="categorias" name="categorias" value="Categorías" class="submit" onclick="location.href='?c=pedidos&a=viewcategorias';"/>
+            <input type="button" id="categorias" name="categorias" value="Orden" class="submit" onclick="location.href='?c=pedidos&a=vieworden';"/>
+            <input type="button" id="login" name="login" value="Iniciar Sesión" class="submit" onclick="location.href='?c=login';"/>
+            <input type="image" src="assets/images/carrito.png" name="carrito" class="carrito"/>
+
+             <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+        <div class="submitLog">
+    	<p>Bienvenid@ <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" id="logout" name="logout">logout</a> </p>
+        </div>
+    <?php endif ?>
+
+         </form>
+         
+    </header>
 		
 		<!-- CATEGORIA ORIGINALS -->
 		<div class="combo">
