047a79ba8c5857f34ee10268f2c724115be1f653
Melvin Robles <56745314+Melvin-Robles@users.noreply.github.com>
2022-05-12 23:23:46 -0600

    Intento de tabla

--- mvc/views/pedidos/orden.php
+++ mvc/views/pedidos/orden.php
@@ -1,3 +1,32 @@
-<?php
-echo "Hola";
-?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Orden</title>
+      
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/bootstrap.min.js" ></script>
+		<style><?php include "assets/css/estiloscategoria.css" ?></style>
+	</head>
+	
+	<body>
+		<div class="container">		
+			
+			<a href="" class="btn btn-primary">Agregar</a>
+			
+			<br />
+			<br />
+			<div class="table-responsive">
+				<table width="80%" class="table">
+					<thead>
+						<tr class="table-primary">
+							<th>Productos</th>
+							<th>Precio</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
